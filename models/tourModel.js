52a8c4cb0753418fd0b9a6e6d2855d7e2d143606
Eduardo FM <eduardomarcos_@outlook.com>
2020-01-10 14:19:44 -0200

    created import-dev-data script

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,17 +3,56 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour name is required!'],
-    unique: true
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true
   },
-  rating: {
+  ratingsAverage: {
     type: Number,
     default: 4.5
   },
   price: {
     type: Number,
-    required: [true, 'Price is required!']
-  }
+    required: [true, 'A tour must have a price']
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    trim: true
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  priceDiscount: Number,
+
+  summary: {
+    type: String,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: [true, 'A tour must have a description'],
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
