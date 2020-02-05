480401feb0f95f64fce135c5e7fab2c846149029
Eduardo FM <eduardomarcos_@outlook.com>
2020-02-05 17:57:10 -0200

    implemented route error handling on app.js

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `Can not find ${req.originalUrl} on this server!`
+  });
+});
+
 // Start server
 
 module.exports = app;
