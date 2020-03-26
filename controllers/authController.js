1ae841421f06b9836c3eaacefdb1c989c2dc264b
Eduardo FM <eduardomarcos_@outlook.com>
2020-03-25 21:37:41 -0300

    final adjustments

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,22 +12,18 @@ const signToken = id => {
   });
 };
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
-  const cookieOptions = {
+  res.cookie("jwt", token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
-  };
+    httpOnly: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https"
+  });
 
-  if (req.secure || res.headers("x-fowarded-proto") === "https")
-    cookieOptions.secure = true;
-
-  res.cookie("jwt", token, cookieOptions);
-
-  //remove the password of output
+  // Remove password from output
   user.password = undefined;
 
   res.status(statusCode).json({
@@ -75,6 +71,8 @@ exports.signup = catchAsync(async (req, res, next) => {
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
+  console.log("passou aqui");
+  console.log(email, password);
 
   if (!email || !password) {
     return next(
@@ -90,7 +88,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
   //const token = signToken(user._id);
 
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 
   // res.status(200).json({
   //   status: 'success',
@@ -246,7 +244,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   //4)log the user in, send the JWT to the client
   //const token = signToken(user._id);
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
   // res.status(200).json({
   //   status: 'success',
   //   token
@@ -274,7 +272,7 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   //4) log the user in, send the JWT to user
 
   //const token = signToken(user._id);
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
   // res.status(200).json({
   //   status: 'success',
   //   token
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,4 @@
-const AppError = require('./../utils/appError');
+const AppError = require("./../utils/appError");
 
 const handleCastErrorDb = err => {
   const message = `Invalid ${err.path}: ${err.value}`;
@@ -14,22 +14,22 @@ const handleDuplicateErrorDB = err => {
 
 const handleValidationErrorDB = err => {
   const msgErrorArray = Object.values(err.errors).map(el => el.message);
-  const message = `Invalid fields ${msgErrorArray.join('. ')}`;
+  const message = `Invalid fields ${msgErrorArray.join(". ")}`;
   return new AppError(message, 400);
 };
 
 const handleJWTerror = err => {
-  if (err.name === 'JsonWebTokenError')
-    return new AppError('Invalid token. Please log in again', 401);
-  if (err.name === 'TokenExpiredError')
-    return new AppError('Expired token. Please log in again', 401);
+  if (err.name === "JsonWebTokenError")
+    return new AppError("Invalid token. Please log in again", 401);
+  if (err.name === "TokenExpiredError")
+    return new AppError("Expired token. Please log in again", 401);
 };
 
 /*************************DEVELOPMENT ERROR********************************* */
 /* eslint-disable no-else-return */
 const sendErrDev = (err, req, res) => {
   //A)API
-  if (req.originalUrl.startsWith('/api')) {
+  if (req.originalUrl.startsWith("/api")) {
     return res.status(err.statusCode).json({
       status: err.status,
       error: err,
@@ -38,19 +38,19 @@ const sendErrDev = (err, req, res) => {
     });
   }
   //B)RENDERED WEBSITE
-  //console.log(err);
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: err.message
+  console.log(err);
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong!",
+    msg: err.stack
   });
 };
 
 /*****************************PRODUCTION ERROR************************************ */
 const sendErrProd = (err, req, res) => {
   //operation trusted error: send the message to the client
-  //console.error(err);
+  
   //A) API
-  if (req.originalUrl.startsWith('/api')) {
+  if (req.originalUrl.startsWith("/api")) {
     if (err.isOperational) {
       return res.status(err.statusCode).json({
         status: err.status,
@@ -61,46 +61,46 @@ const sendErrProd = (err, req, res) => {
     // unstrusted programming error or unknown error: Doest not leak to client
     //send generic message to client
     return res.status(404).json({
-      status: 'Error',
-      message: 'Something went very wrong :( !'
+      status: "Error",
+      message: "Something went very wrong!"
     });
   }
   //B) RENDERED WEBSITE
   //operation trusted error: send the message to the client
-  //onsole.error(err);
+  //console.error(err);
   if (err.isOperational) {
-    return res.status(err.statusCode).render('error', {
-      title: 'Something went wrongaaaaaaaaaa!',
+    return res.status(err.statusCode).render("error", {
+      title: "Something went wrongaaaaaaaaaa!",
       msg: err.message
     });
   }
   // unstrusted programming error or unknown error: Doest not leak to client
   //send generic message to client
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!!',
-    msg: 'Please Try Again Later :('
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong!!",
+    msg: "Please Try Again Later :("
   });
 };
 module.exports = (err, req, res, next) => {
   //console.log(err);
   err.statusCode = err.statusCode || 500;
   //console.log(err.statusCode);
-  err.status = err.status || 'error';
+  err.status = err.status || "error";
   //console.log(err.status);
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     sendErrDev(err, req, res);
   }
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     let error = { ...err };
     //console.log(error.name);
     error.message = err.message;
-    if (error.name === 'CastError') error = handleCastErrorDb(error);
+    if (error.name === "CastError") error = handleCastErrorDb(error);
     if (error.code === 11000) error = handleDuplicateErrorDB(error);
-    if (error.name === 'ValidationError')
+    if (error.name === "ValidationError")
       error = handleValidationErrorDB(error);
-    if (error.name === 'JsonWebTokenError') error = handleJWTerror(error);
-    if (error.name === 'TokenExpiredError') error = handleJWTerror(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTerror(error);
+    if (error.name === "TokenExpiredError") error = handleJWTerror(error);
     //console.log(error);
     sendErrProd(error, req, res);
   }
