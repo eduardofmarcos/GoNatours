80e49e328b214accb35b0eef405ecc3de9afaecd
Eduardo FM <eduardomarcos_@outlook.com>
2020-03-26 12:48:27 -0300

    fix alert-07

--- public/js/index.js
+++ public/js/index.js
@@ -86,4 +86,6 @@ if (bookBtn) {
 }
 
 const alertMessage = document.querySelector("body").dataset.alert;
-if (alertMessage) showAlert("success", alertMessage, 10);
+if (alertMessage) {
+  showAlert("success", alertMessage, 10);
+}
