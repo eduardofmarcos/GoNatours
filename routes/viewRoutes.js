aebb5f14464a0a3ef4b9f41beec56b5c554d5001
Eduardo FM <eduardomarcos_@outlook.com>
2020-03-26 11:38:29 -0300

    fix alert-03

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const viewController = require('../controllers/viewController');
-const authController = require('./../controllers/authController');
-const bookingController = require('./../controllers/bookingController');
+const express = require("express");
+const viewController = require("../controllers/viewController");
+const authController = require("./../controllers/authController");
+const bookingController = require("./../controllers/bookingController");
 
 const router = express.Router();
 
-router.use(viewController.alert)
+router.use(viewController.alerts);
 
 router.get(
-  '/',
- // bookingController.createBookingCheckout,
+  "/",
+  // bookingController.createBookingCheckout,
   authController.isLoogedIn,
   viewController.getOverview
 );
 
-router.get('/tour/:slug', authController.isLoogedIn, viewController.getTour);
-router.get('/login', authController.isLoogedIn, viewController.getLoginForm);
-router.get('/me', authController.protect, viewController.getAccount);
-router.get('/mytours', authController.protect, viewController.getMyTours);
+router.get("/tour/:slug", authController.isLoogedIn, viewController.getTour);
+router.get("/login", authController.isLoogedIn, viewController.getLoginForm);
+router.get("/me", authController.protect, viewController.getAccount);
+router.get("/mytours", authController.protect, viewController.getMyTours);
 
 module.exports = router;
