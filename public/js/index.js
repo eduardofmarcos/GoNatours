c1b478bb251817b28adb096483cf95ce972637f8
Eduardo FM <eduardomarcos_@outlook.com>
2020-03-26 12:58:21 -0300

    fix alert-08

--- public/js/index.js
+++ public/js/index.js
@@ -85,7 +85,5 @@ if (bookBtn) {
   });
 }
 
-const alertMessage = document.querySelector("body").dataset.alert;
-if (alertMessage) {
-  showAlert("success", alertMessage, 10);
-}
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
